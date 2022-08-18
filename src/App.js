a3cf4d36fd66769754d29aff3600592c9c910d8f
Kenan Sultanic <kenansultanic0805@gmail.com>
2022-08-18 17:35:53 +0200

    add files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,68 @@
-import logo from './logo.svg';
+/* eslint-disable no-unused-vars */
 import './App.css';
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function App() {
+
+  const [message, setMessage] = useState('Test')
+  const [name, setName] = useState('')
+  const [age, setAge] = useState('')
+  const [allData, setAllData] = useState([])
+
+  useEffect(() => {
+      axios.get('http://localhost:3001/test', {
+      }).then(response => {
+          setMessage(response.data)
+      }, error => {
+          console.log(error)
+      })
+  
+  }, [])
+
+  const send = () => {
+    axios.post('http://localhost:3001/post', {
+      name: name,
+      age: age
+    }).then(response => {
+        setMessage("Saved to database")
+        console.log(response)
+    }, error => {
+        console.log(error)
+    })
+  }
+
+  const getAll = () => {
+    axios.get('http://localhost:3001/get', {
+    }).then(response => {
+        setAllData(response.data)
+        console.log(response)
+    }, error => {
+        console.log(error)
+    })
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>
+          {
+            message
+          }
+        </h3>
+        <br/>
+        <input type={'text'} placeholder={'Enter your name'} onChange={e => setName(e.target.value)}/><br/>
+        <input type={'number'} placeholder={'Enter your age'} onChange={e => setAge(e.target.value)}/>
+        <button onClick={send}>Save</button>
+        <button onClick={getAll}>Get all data</button>
+
+        <ul>
+          {
+            allData.map(obj =>  <li key={obj._id}>{obj.name} {obj.age}</li>)
+          }
+        </ul>
+        <button onClick={() => setAllData([])}>Delete all data</button>
       </header>
     </div>
   );
